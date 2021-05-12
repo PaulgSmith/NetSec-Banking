b5169c155c4a84d06631f2226a4a7ca84ce71cd1
Paul <PaulSmith.Contactinfo@gmail.com>
2021-05-12 01:59:13 -0700

    Night 2

---
+++ bank.sql
@@ -0,0 +1,34 @@
+-- Create Customer Table
+CREATE TABLE IF NOT EXISTS `customer` (
+  `username` VARCHAR(255) NOT NULL,
+  `first_name` VARCHAR(255) NOT NULL,
+  `last_name` VARCHAR(255) NOT NULL,
+  `password` VARCHAR(255) NOT NULL,
+  `address` VARCHAR(255) NOT NULL,
+  PRIMARY KEY (username)
+);
+
+--Create Account Table 
+CREATE TABLE IF NOT EXISTS `account` (
+  `account_number` int  NOT NULL AUTO_INCREMENT,
+  `account_name` VARCHAR(255) NOT NULL,
+  `account_type` VARCHAR(255) NOT NULL,
+  `balance`  FLOAT DEFAULT 0.00,
+  `username` VARCHAR(255) NOT NULL,
+  PRIMARY KEY (account_number),
+  FOREIGN KEY (username) REFERENCES customer(username)
+);
+
+-- Customer Login
+SELECT password FROM customer WHERE username = "paul";
+
+-- Register Customer
+INSERT INTO customer(username, last_name, first_name, password, address) 
+VALUES ("Dehobo","Paul", "Smith", "1234", "1234 main st");
+
+-- Create Account
+INSERT INTO account(account_name, account_type, balance, username)
+VALUES ('Checking', 'c', '10.00', (SELECT username FROM customer WHERE username = 'Dehobo'));
+
+-- View Accounts
+SELECT * FROM account WHERE username = "Dehobo";
--- index.js
+++ index.js
@@ -5,8 +5,38 @@ const xssFilters = require('xss-filters');
 const express = require("express");
 const session = require('client-sessions');
 const fs = require("fs");
+const mysql = require('mysql');
 
-const xml2js = require("xml2js")
+const app = express();
+
+const dbCredentials = {
+    username: "db1",
+    password: "Hfd%4g3hf&^FGH",
+    host: "localhost",
+    database: "bank"
+};
+
+const connection = mysql.createConnection({
+    host: dbCredentials.host,
+    user: dbCredentials.username,
+    password: dbCredentials.password,
+    database: dbCredentials.database,
+    multipleStatements: true
+});
+
+// connection.query("", function (err, result) {
+//     if (err) {
+//         console.log(err);
+//         req.info = `There was a problem with the query`;
+//         req.show_results = ``;
+//         return res.render('results', { info: `${req.info}`, show_results: `${req.show_results}` });
+//     } else {
+//         req.info = `Success`;
+//         req.show_results = ``;
+//         return res.render('results', { info: `${req.info}`, show_results: `${req.show_results}` });
+//     }
+// });
+
 
 // Needed to parse the request body
 //Note that in version 4 of express, express.bodyParser() was
@@ -32,9 +62,6 @@ function requireLogin(req, res, next) {
     }
 }
 
-
-
-
 app.use(function (req, res, next) {
     if (req.session && req.session.user) {
         User.findOne({ email: req.session.user.email }, function (err, user) {
@@ -52,6 +79,9 @@ app.use(function (req, res, next) {
     }
 });
 
+// use express static middleware to serve static files
+app.use(express.static(__dirname));
+
 // account - bank account
 // customer - user account
 
@@ -63,39 +93,147 @@ app.get("/", requireLogin, function (req, res) {
 });
 
 app.get("/login", function (req, res) {
-    res.render("");
+    let page = '<html>';
+    page += '<head>'
+    page += '<title>Login</title>'
+    page += '<link rel="stylesheet" type ="text/css" href="style.css" />'
+    page += '</head>'
+    page += '<center>'
+    page += '<h1>Welcome to Bank App</h1>'
+    page += '</center>'
+    page += '<body>'
+    page += '<div class="form-container">'
+    page += '<ul class="list">'
+    page += '<li><h2>Member Login</h2></li>'
+    //TODO FIX ERROR PASSING
+    if (req.body.error) {
+        page += `<h1>Error! Try again.<h1>`
+    }
+    page += '<li><form action="/login" method="POST">'
+    page += '<label for="username">Username:</label>'
+    page += '<input type="text" name="username" placeholder="username" /></li>'
+    page += '<li><label for="password">Password:</label>'
+    page += '<input type="password" name="password" placeholder="********" /></li>'
+    page += '<li><input type="button" value="Submit" onclick ="submit()"> </li></form>'
+    page += '</ul>'
+    page += '</div>'
+    page += '</body>'
+    page += '</html>'
+    res.send(page);
 });
 
 app.post("/login", function (req, res) {
-
+    let sql = `SELECT password ` +
+        `FROM customer WHERE username = "${req.body.username}"`
+    connection.query(sql, function (err, result) {
+        if (err) {
+            console.log(err);
+            req.body.error = true;
+            return res.redirect("/login");
+        } else {
+            if (result[0] && result[0].password === req.body.password) {
+                req.body.error = false;
+                console.log("yes");
+                req.show_results = `You have logged in`;
+                res.redirect("/view_account");
+            }
+            else {
+                //TODO FIX ERROR PASSING
+                req.body.error = true;
+                console.log("No")
+                res.redirect("/login");
+            }
+        }
+    });
 });
+
+
 
 
 app.get("/register_customer", function (req, res) {
-    res.render("");
+    let sql = `INSERT INTO customer(username, last_name, first_name, password, address) ` +
+        `VALUES ("${req.body.username}","${req.body.firstname}", "${req.body.lastname}", "${req.body.password}", "${req.body.address}")`
+
+
+    connection.query(sql, function (err, result) {
+        if (err) {
+            console.log(err);
+            req.error = ``;
+            req.show_results = `Error creating user account. Please try again.`;
+            return res.render();
+        } else {
+            req.show_results = ``;
+            return res.render();
+        }
+    });
 });
 
+//TODO START HERE
 app.get("/view_account", requireLogin, function (req, res) {
     let page = "<html>";
-    page += `<title>Welcome ${userName} </title>`;
-    page += `<h2>Welcome ${userName}</h2>`;
+    page += `<title>Welcome Bobby Jones </title>`;
+    page += `<h2>Welcome Bobby Jones</h2>`;
 
-    res.render("");
+    let sql = `SELECT * FROM account WHERE username = "(${req.body.username})"`
+
+
+    connection.query(sql, function (err, result) {
+        if (err) {
+            console.log(err);
+            req.error = ``;
+            req.show_results = `cant find accounts for shiet`;
+            return res.render();
+        } else {
+            req.show_results = ``;
+            return res.render();
+        }
+        res.render(page);
+    });
 });
 
 app.get("/create_account", function (req, res) {
-    res.render("");
+    let sql = `INSERT INTO account(account_number, account_name, account_type, balance, username) ` +
+        `VALUES ('${req.body.account_name}', '${req.body.account_type}', '${req.body.balance}', "(SELECT username FROM customer WHERE username =${req.body.username})")`
+
+
+    connection.query(sql, function (err, result) {
+        if (err) {
+            console.log(err);
+            req.error = ``;
+            req.show_results = `Error creating account. Please try again.`;
+            return res.render();
+        } else {
+            req.show_results = ``;
+            return res.render();
+        }
+    });
 });
 
-//Transfer, deposit, withdraw
+//Transfer (within own user accounts), deposit, withdraw 
 app.get("/account_actions", function (req, res) {
     //TODO JS
-    res.render("");
+
+    let sql = `UPDATE account WHERE balance = '${req.body.balance}'`
+
+    connection.query(sql, function (err, result) {
+        if (err) {
+            console.log(err);
+            req.error = ``;
+            req.show_results = `Error updating balance. Please try again.`;
+            return res.render();
+        } else {
+            req.show_results = ``;
+            return res.render();
+        }
+    });
 });
+
 
 app.get('/logout', function (req, res) {
 
-    alert("You have been logged out. Have a nice day!")
+    alert("You have been logged out. Have a nice day!");
     req.session.reset();
     res.redirect('/');
 });
+
+app.listen(3000);
